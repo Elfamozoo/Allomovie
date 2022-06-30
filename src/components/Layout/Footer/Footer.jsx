71c3043338e0d49dab25292c355453c622b25ee5
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-06-30 16:47:40 +0200

    Navbar et Footer en cours de construction

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiFillGithub, AiFillGitlab } from 'react-icons/ai';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer class='p-4 bg-white sm:p-6 dark:bg-gray-800'>
+			<hr class='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
+			<div class='sm:flex sm:items-center sm:justify-between'>
+				<span class='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+					© 2022{' '}
+					<a
+						href='https://illyeszerga-portfolio.netlify.app/'
+						class='hover:underline'>
+						Zerga Illyes
+					</a>
+					. All Rights Reserved.
+				</span>
+				<div class='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
+					<a
+						href='https://github.com/Elfamozoo'
+						class='text-gray-500 hover:text-gray-900 dark:hover:text-white'>
+						<AiFillGithub />
+					</a>
+					<a
+						href='https://gitlab.com/Elfamozoo'
+						class='text-gray-500 hover:text-gray-900 dark:hover:text-white'>
+						<AiFillGitlab />
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../../assets/logo.svg';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
 
 const Navbar = () => {
 	return (
@@ -13,8 +15,13 @@ const Navbar = () => {
 					<div class='flex md:order-2'>
 						<button
 							type='button'
-							class='text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800'>
-							Inscription
+							class='hidden md:block text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none rounded-full focus:ring-indigo-700 font-medium text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800'>
+							Connexion
+						</button>
+						<button
+							type='button'
+							class='visible lg:invisible text-gray-900 bg-white border border-gray-300 focus:outline-none rounded-full hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-2.5 py-2.5 mr-3 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>
+							<FcGoogle />
 						</button>
 						<button
 							data-collapse-toggle='mobile-menu-4'
@@ -23,26 +30,7 @@ const Navbar = () => {
 							aria-controls='mobile-menu-4'
 							aria-expanded='false'>
 							<span class='sr-only'>Open main menu</span>
-							<svg
-								class='w-6 h-6'
-								fill='currentColor'
-								viewBox='0 0 20 20'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									fill-rule='evenodd'
-									d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-									clip-rule='evenodd'></path>
-							</svg>
-							<svg
-								class='hidden w-6 h-6'
-								fill='currentColor'
-								viewBox='0 0 20 20'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									fill-rule='evenodd'
-									d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-									clip-rule='evenodd'></path>
-							</svg>
+							<AiOutlineMenu />
 						</button>
 					</div>
 					<div
